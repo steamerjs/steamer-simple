017a977222feee5b8f11193161edb3a1e1cb0410
lcxfs1991@gmail.com <lcxfs1991@gmail.com>
2017-04-13 11:47:45 +0800

    add css and image cdn

--- config/steamer.config.js
+++ config/steamer.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     "webserver": "//localhost:9000/",
     "cdn": "//localhost:8000/",
-    "cssCdn": "//localhost:7000/",
+    "cssCdn": "//localhost:8000/",
     "imgCdn": "//localhost:8000/",
     "port": "9000",
     "route": "/news/",
